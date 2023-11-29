958425b1a3cc6a16f05dd20a5aba5150e60c36f1
premasajan <premasajan25@gmail.com>
2023-11-29 17:24:21 +0530

    add

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-Let sections = document.querySelectorAll('section');
-Let navLinks = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () =>{
     sections.forEach(sec =>{
-        Let top = window.scrollY;
-        Let offset = sec.offsetTop - 150;
-        Let height = sec.offsetHeight;
-        Let id = sec.getAttributr('id');
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttributr('id');
 
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
@@ -14,5 +25,34 @@ window.onscroll = () =>{
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
-    };
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+    
+
 };
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact-form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+const typed = new Typed('.multi-text', {
+    strings: ['Frontend Developer','UI/UX Engineer','Softwate Engineer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
