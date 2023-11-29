6b36ee3652af372e516b92c12fd49b0ec0d89fe5
premasajan <premasajan25@gmail.com>
2023-11-29 22:07:49 +0530

    add

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ menuIcon.onclick = () => {
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll = () =>{
-    sections.forEach(sec =>{
+window.onscroll = () => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttributr('id');
+        let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
             navLinks.forEach(links => {
